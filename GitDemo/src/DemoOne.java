7ab462859a43bdfaeaadee8d9c29d29ee0af6f7b
Vikram kundu <Vikram kundu@LAPTOP-AOMNSM4P>
2020-10-05 17:49:45 +0530

    new function added

--- GitDemo/src/DemoOne.java
+++ GitDemo/src/DemoOne.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class DemoOne {
+	public int calculate(int orders)
+	{int sum = 0; 
+    
+  while (orders != 0) 
+  { 
+      sum = sum + orders % 10; 
+      orders = orders/10; 
+  } 
+
+return sum;
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DemoOne d=new DemoOne();
 
+Scanner s1= new Scanner(System.in);
+System.out.print("Enter the number of products" );
+
+int n=s1.nextInt();
+
+
+
+System.out.println(d.calculate(n));
 	}
 
 }
